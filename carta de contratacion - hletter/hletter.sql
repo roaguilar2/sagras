fe17aac6bba86df2c90993f87b9847ff8838c9c2
roaguilar2 <61738365+roaguilar2@users.noreply.github.com>
2022-04-08 09:23:14 -0400

    modulos por revision

---
+++ carta de contratacion - hletter/hletter.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-04-2022 a las 15:02:00
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sagracom_alberto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `hletter`
+--
+
+CREATE TABLE `hletter` (
+  `hletterId` int(11) NOT NULL,
+  `hletterName` varchar(100) DEFAULT NULL,
+  `hletterStatus` int(11) NOT NULL DEFAULT 1,
+  `hletterTime` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `hletter`
+--
+ALTER TABLE `hletter`
+  ADD PRIMARY KEY (`hletterId`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `hletter`
+--
+ALTER TABLE `hletter`
+  MODIFY `hletterId` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
