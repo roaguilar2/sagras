d3fd40928d3c7ceb28b3ca4fc7dafd14fc23a6d3
roaguilar2 <61738365+roaguilar2@users.noreply.github.com>
2022-07-08 08:25:15 -0400

    botones de sistema

---
+++ BOTONES DE LUPA, LAPIZ Y BORRADOR.php
@@ -0,0 +1,5 @@
+BOTONES DE LUPA, LAPIZ Y BORRADOR SIN PERMISOS DE GP
+LA LUPA
+
+
+<a href="../c/project.php?m=index2&serviceId=<?php echo $cp -> serviceId; ?>"<i class='material-icons blue-text'>search</i></a>
