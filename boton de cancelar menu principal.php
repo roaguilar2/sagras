7a59b2c1f9d1251e8b8b2fd004e6e54c9fc7be78
roaguilar2 <61738365+roaguilar2@users.noreply.github.com>
2022-07-12 16:10:26 -0400

    boton cancelar

---
+++ boton de cancelar menu principal.php
@@ -0,0 +1,5 @@
+<!-- boton cancelar que lleva al menu principal -->
+
+<a href="../c/main.php?m=menu" class="tooltipped right " data-position="left" data-tooltip="Cerrar">
+                                        <i class="material-icons blue-text">cancel</i>
+                                    </a>
