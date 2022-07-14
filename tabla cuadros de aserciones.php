f2be8d9a41d7c4e24779b4d3411f47b0c015485c
roaguilar2 <61738365+roaguilar2@users.noreply.github.com>
2022-07-14 08:47:19 -0400

    ronald

---
+++ tabla cuadros de aserciones.php
@@ -0,0 +1,195 @@
+<!-- tabla q contiene las actividades en un desplegable  -->
+
+<div class="col s12 m12 l12 ">
+                                            <ul class="collapsible">
+                                                <li>
+                                                    <div class="collapsible-header grey lighten-4"><i class="material-icons blue-text">list</i>Descripci&oacute;n</div>
+                                                    <div class="collapsible-body">
+                                                        <div class="row">
+
+
+                                                            <table class="" style="width:100%;">
+                                                                <tbody>
+<tr>
+
+<td class="center grey lighten-4">
+                                                                                    <div class='q-items b-b-white'> <a href='#' class="blue-text">
+                                                                                    C
+                                                                                         </a></div>
+</td>                                                                                         
+<td class="center grey lighten-4">
+                                                                                    <div class='q-items b-b-white'> <a href='#' class="blue-text">
+                                                                                    A
+                                                                                         </a></div>
+</td>
+<td class="center grey lighten-4">
+                                                                                    <div class='q-items b-b-white'> <a href='#' class="blue-text">
+                                                                                    E/O
+                                                                                         </a></div>
+</td>
+<td class="center grey lighten-4" >
+                                                                                    <div class='q-items b-b-white'> <a href='#' class="blue-text">
+                                                                                    CO
+                                                                                         </a></div>
+</td>
+<td class="center grey lighten-4" >
+                                                                                    <div class='q-items b-b-white'> <a href='#' class="blue-text">
+                                                                                    RO
+                                                                                         </a></div>
+</td>
+<td class="center grey lighten-4" >
+                                                                                    <div class='q-items b-b-white'> <a href='#' class="blue-text">
+                                                                                    VA
+                                                                                         </a></div>
+</td>
+<td class="center grey lighten-4" >
+                                                                                    <div class='q-items b-b-white'> <a href='#' class="blue-text">
+                                                                                    PD
+                                                                                         </a></div>
+</td>
+<td class="center grey lighten-4" style="width:3%;">
+</td>
+</tr>
+                                                                    
+
+                                                                        <tr>
+                                                                           
+                                                                            
+
+
+                                                                                
+                                                                            
+                                                                            <td class="  grey lighten-4" style="width:3%;background-color:white">
+
+                                                                                <a class=" tooltipped" data-tooltip="Asercion  1 C">
+
+
+                                                                                    <label>
+                                                                                        <input value="<?php echo $mmodelo->aId; ?>" type="hidden" name="preguntasC1[]" />
+                                                                                        <input class="filled-in "   value="1" type="checkbox" name="pc[<?php echo $mmodelo->aId; ?>]" />
+                                                                                        <span class="blue-grey-text text-darken-2"> </span>
+                                                                                    </label>
+
+
+
+                                                                                </a>
+
+
+
+                                                        </div>
+                                                        </td>
+                                                        <td class="center  grey lighten-4" style="width:3%; background-color:#cfd8dc">
+                                                            <div>
+                                                                <a class=" tooltipped" data-tooltip="Asercion 2 A">
+                                                                    <label>
+                                                                        <input value="<?php echo $mmodelo->aId; ?>" type="hidden" name="preguntasC2[]" />
+                                                                        <input class="filled-in "  type="checkbox" value="1"  name="pca[<?php echo $mmodelo->aId; ?>]" />
+                                                                        <span class="blue-grey-text text-darken-2"></span>
+                                                                    </label>
+                                                                     
+
+                                                                </a>
+
+
+                                                            </div>
+                                                        </td>
+                                    
+                                                        <td class="center grey lighten-4" style="width:3%;background-color:#cfd8dc">
+                                                            <div>
+                                                                <a class=" tooltipped" data-tooltip="Asercion 3 E/O">
+                                                                    <label>
+                                                                        <input value="<?php echo $mmodelo->aId; ?>" type="hidden" name="preguntasC3[]" />
+                                                                        <input class="filled-in "  type="checkbox" name="pcb[<?php echo $mmodelo->aId; ?>]" />
+                                                                        <span class="blue-grey-text text-darken-2"></span>
+                                                                    </label>
+
+                                                                </a>
+                                                            </div>
+                                                        </td>
+
+                                                        <td class="center  grey lighten-4" style="width:3%;background-color:#cfd8dc">
+                                                            <div>
+                                                                <a class=" tooltipped" data-tooltip="Asercion 4 CO">
+
+                                                                    <label>
+                                                                        <input value="<?php echo $mmodelo->aId; ?>" type="hidden" name="preguntasC4[]" />
+                                                                        <input class="filled-in "  type="checkbox" name="pcc[<?php echo $mmodelo->aId; ?>]" />
+                                                                        <span class="blue-grey-text text-darken-2"></span>
+                                                                    </label>
+
+
+                                                                </a>
+
+                                                            </div>
+                                                        </td>
+
+                                                        <td class="center  grey lighten-4" style="width:3%;background-color:white">
+                                                            <div>
+                                                                <a class=" tooltipped" data-tooltip="Asercion 5 RO">
+                                                                    <label>
+                                                                        <input value="<?php echo $mmodelo->aId; ?>" type="hidden" name="preguntasC5[]" />
+                                                                        <input class="filled-in "  type="checkbox" name="pcd[<?php echo $mmodelo->aId; ?>]" />
+                                                                        <span class="blue-grey-text text-darken-2"> </span>
+                                                                    </label>
+
+                                                            </div>
+
+                                                            </a>
+                                                    </div>
+                                                    </td>
+
+                                                    <td class="center  grey lighten-4" style="width:3%; background-color:#cfd8dc">
+                                                        <div>
+                                                            <a class=" tooltipped" data-tooltip="Asercion 6 VA">
+                                                                <label>
+                                                                    <input value="<?php echo $mmodelo->aId; ?>" type="hidden" name="preguntasC6[]" />
+                                                                    <input class="filled-in "  type="checkbox" name="pce[<?php echo $mmodelo->aId; ?>]" />
+                                                                    <span class="blue-grey-text text-darken-2"></span>
+                                                                </label>
+
+                                                        </div>
+                                                        </a>
+                                        </div>
+                                        </td>
+
+                                        <td class="center  grey lighten-4" style="width:3%;background-color:#cfd8dc">
+                                            <div>
+
+                                                <a class=" tooltipped" data-tooltip="Asercion 7 PD">
+                                                    <label>
+                                                        <input value="<?php echo $mmodelo->aId; ?>" type="hidden" name="preguntasC7[]" />
+                                                        <input class="filled-in "  type="checkbox" name="pcf[<?php echo $mmodelo->aId; ?>]" />
+                                                        <span class="blue-grey-text text-darken-2"></span>
+                                                    </label>
+                                                </a>
+
+                                            </div>
+                                        </td>
+
+
+
+                                        <td class="center blue-grey lighten-3" style="width:3%;">
+                                            <a class=" tooltipped" href="../c/project.php?m=revision2&p=p&c=<?php echo $c;?>&amId=<?php echo $amId;?>&serviceId=<?php echo $serviceId;?>&md=<?php echo $md;?>&rubroId=<?php echo $rubroId;?>&frecuencia=<?php echo $frecuencia;?>" data-tooltip="Asignar tarea por Frecuencia">
+                                                <i class="material-icons blue-grey-text">edit</i>
+
+                                            </a>
+                                        </td>
+                                        <td class="center blue-grey lighten-3" style="width:3%;">
+                                            <a class=" tooltipped" href="../c/project.php?m=revision2&p=p&c=<?php echo $c;?>&amId=<?php echo $amId;?>&serviceId=<?php echo $serviceId;?>&md=<?php echo $md;?>&rubroId=<?php echo $rubroId;?>&frecuencia=<?php echo $frecuencia;?>" data-tooltip="Asignar tarea por Frecuencia">
+                                                <i class="material-icons blue-grey-text">edit</i>
+
+                                            </a>
+                                        </td>
+
+                                    <?php } ?>
+
+                                    </tbody>
+                                    </table>
+
+                                    </div>
+                                </div>
+
+                                </li>
+                                </ul>
+                            </div>
+                            <!-- final tabla que contiene actividades-->
