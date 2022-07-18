e96b9e286c9dbd44ba075d557e956223552c49ac
ronald aguilar <ronald20313152@gmail.com>
2022-07-18 15:33:04 -0400

    modalmat1

---
+++ modalMat1.php
@@ -0,0 +1,57 @@
+<!-- Oc 1 -->
+<div id="mat1" class="modal " style="width:80%;">
+            <div class="modal-header" style="background-color:#607d8b;">
+            <div class="row" >
+                    <div class="col s12 m-b-5 m-t-20 m-b-20 m-l-20 ">
+                        <h6><a href="#!" class="breadcrumb white-text">Agregar </a>
+                        <a href="#!" class="breadcrumb white-text ">Punto de Control</a>
+                        <a href="#!" class="tooltipped modal-close right modal-trigger" data-position="left" data-tooltip="Cerrar" display:none;>
+                        </a></h6>
+                    </div>
+                </div>
+            </div>
+      
+ <div class="modal-content m-t--10 white" >
+ <form id="form_validation" action="../c/project.php?m=readDb&c=<?php echo $c;?>&amId=<?php echo $amId;?>&serviceId=<?php echo $serviceId;?>&md=<?php echo $md;?>&a=1" method="post">               
+
+ 
+
+                                <div class="row">                        
+                                                    <div class="col s12 m-t-20">
+                                                        <label>
+                                                            <input type="checkbox" class="validate filled-in" required="" />
+                                                            <span>Estoy de acuerdo con la informaci&oacute;n suministrada!</span>
+                                                        </label>
+                                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col s3 m-b-10">
+                                        <button type="submit" class="btn blue waves-effect tooltipped" data-position="top" data-tooltip="Guardar">
+                                            <i class="material-icons">save</i>
+                                        </button>
+                                    </div>
+                                </div>    
+
+</form>
+
+                <p>
+
+                </p>
+                <div class="m-t-20"></div>
+         
+     
+        
+            </div>
+
+    <div class="modal-footer">
+      
+     <div class="row">
+                                <div class="col s12 m-t--10 ">
+                                    <span class="blue-grey-text font-14 left p-l-10">&copy; 2020 | SAGRA GP | S.C Aguilar, Godoy y Asociados</span>
+                                    <span class="blue-grey-text font-14 right p-r-10 hide-on-small-only">Versi&oacute;n: 1.0.0</span>
+                                </div>
+  </div>
+    </div>
+    <div class="m-t--20"></div>
+</div>
+<!-- OC1     --> 
