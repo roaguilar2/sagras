6c3b615a36ab922bb8105a098884c6a500921d5b
ronald aguilar <ronald20313152@gmail.com>
2022-07-18 17:16:10 -0400

    ronald

---
+++ boton funcion modal.php
@@ -0,0 +1,6 @@
+<div class="row">
+                                                                                    
+            <div class="col s3 m-b-10">
+                <a class="waves-effect waves-light btn blue-grey modal-trigger" href="#mat1">Beneficios operaciones continuas antes de impuestos</a>                                          
+            </div>
+</div>
